adb67d4e0ba26793915ea3a5f95d3e60deab4fa7
Duska12 <duska.dujakovic@gmx.ch>
2021-12-13 19:48:28 +0100

    changes

--- eventHandlers.js
+++ eventHandlers.js
@@ -3,27 +3,48 @@ const logger = require('./db/logger.js');
 exports.sendEvent = null;
 
 exports.registerEventHandlers = function (source) {
-    source.addEventListener('Sensor', forwardEvent);
+    source.addEventListener('Gasdurch', handleGas);
+    source.addEventListener('Temperaturdurch', handleTemp);
     // Register more event handlers here
-}
-
-function forwardEvent(event) {
-    // read variables from the event
-    var data = {
-        eventName: event.type,
-        eventData: JSON.parse(event.data).data, // the value of the event
-        deviceId: JSON.parse(event.data).coreid,
-        timestamp: JSON.parse(event.data).published_at
-    };
-
-    try {        
-        //Log the event in the database
-        logger.logOne("MyDB", "Fruitquality", data);
-
-        // send data to all connected clients
-        exports.sendEvent(data);
-    } catch (error) {
-        console.log("Could not handle event: " + JSON.stringify(event) + "\n");
-        console.log(error)
     }
-}
+    
+    function handleGas(event) {
+        // read variables from the event
+        var data = {
+            eventName: event.type,
+            eventData: JSON.parse(event.data).data, // the value of the event
+            deviceId: JSON.parse(event.data).coreid,
+            timestamp: JSON.parse(event.data).published_at
+        };
+    
+        try {        
+            // Log the event in the database
+            logger.logOne("MyDB", "Gasdurchschnitt", data);
+    
+            // send data to all connected clients
+            exports.sendEvent(data);
+        } catch (error) {
+            console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+            console.log(error)
+        }
+    }
+    function handleTemp(event) {
+        // read variables from the event
+        var data = {
+            eventName: event.type,
+            eventData: JSON.parse(event.data).data, // the value of the event
+            deviceId: JSON.parse(event.data).coreid,
+            timestamp: JSON.parse(event.data).published_at
+        };
+    
+        try {        
+            // Log the event in the database
+            logger.logOne("MyDB", "Temperaturdurchschnitt", data);
+    
+            // send data to all connected clients
+            exports.sendEvent(data);
+        } catch (error) {
+            console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+            console.log(error)
+        }
+    }
--- public/fruitquality.js
+++ public/fruitquality.js
@@ -14,30 +14,165 @@ function initSSE() {
 }
 initSSE();
 
+
+// Array, in dem alle empfangenen Gas-Werte gespeichert werden.
+var allMeasurements = [];
+
+// Maximaler Gas Level für die Berechnung des Prozentwerts
+var maxLevel = 500;
+
+// Diese Funktion wird immer dann ausgeführt, wenn ein neues Event empfangen wird.
 function updateVariables(data) {
-    // update the html elements
-    document.getElementById("lastevent").innerHTML = JSON.stringify(data);
-    if (data.eventName === "Sensor") {
-        document.getElementById("counterevent").innerHTML = data.eventData;
+
+    if (data.eventName === "Gasdurch") {
+        // Erhaltenen Wert in der Variable 'lux' speichern
+        var gas = Number(data.eventData);
+        //console.log(lux);
+
+        // Wert am Ende des Arrays 'allMeasurements' hinzufügen
+        allMeasurements.push(gas);
+
+        // Wert in Prozent umrechnen und in 'level' speichern
+        var level = gas * (100 / maxLevel);
+
+        // Farbe des Balkens abhängig von Level festlegen
+        // Liste aller unterstützten Farben: https://www.w3schools.com/cssref/css_colors.asp
+        if (level < 50) {
+            color = "Aqua";
+        } else {
+            color = "Coral";
+        }
+
+        // CSS Style für die Hintergrundfarbe des Balkens
+        var colorStyle = "background-color: " + color + " !important;";
+
+        // CSS Style für die Breite des Balkens in Prozent
+        var widthStyle = "width: " + level + "%;"
+
+        // Oben definierte Styles für Hintergrundfarbe und Breite des Balkens verwenden, um
+        // den Progressbar im HTML-Dokument zu aktualisieren
+        document.getElementById("gaslevel-bar").style = colorStyle + widthStyle;
+
+        // Text unterhalb des Balkens aktualisieren
+        document.getElementById("gaslevel-text").innerHTML = gas + "Gasdurchschnitt"
+
+        // Durchschnittlicher Temperaturwert
+        // sobald ein Temperaturevent gesendet wird, soll er in einer Array gespeichert werden
+        var allTemperature = [];
+
+        // maximaler Temperaturwert
+        var maxTemperatur = 30; 
+
+        if (data.eventName === "Temperaturdurch") {
+                // Erhaltenen Wert in der Variable 'lux' speichern
+                var temp = Number(data.eventData);
+                //console.log(lux);
+        
+                // Wert am Ende des Arrays 'allMeasurements' hinzufügen
+                allMeasurements.push(temp);
+        
+                // Wert in Prozent umrechnen und in 'level' speichern
+                var level = temp * (100 / maxLevel);
+
+           // Farbe des Balkens abhängig von Level festlegen
+        // Liste aller unterstützten Farben: https://www.w3schools.com/cssref/css_colors.asp
+        if (level < 50) {
+            color = "DarkCyan";
+        } else {
+            color = "DarkMagneta";
+        }
+
+        // CSS Style für die Hintergrundfarbe des Balkens
+        var colorStyle = "background-color: " + color + " !important;";
+
+        // CSS Style für die Breite des Balkens in Prozent
+        var widthStyle = "width: " + level + "%;"
+
+        // Oben definierte Styles für Hintergrundfarbe und Breite des Balkens verwenden, um
+        // den Progressbar im HTML-Dokument zu aktualisieren
+        document.getElementById("templevel-bar").style = colorStyle + widthStyle;
+
+        // Text unterhalb des Balkens aktualisieren
+        document.getElementById("templevel-text").innerHTML = temp + "Temperaturdurchschnitt"
+
+
+
+        // Durchschnitt aller bisherigen Messungen berechnen und in 'luxAverage' speichern
+        //var gasSum = 0;
+        //for (var measurement of allMeasurements) {
+            //gasSum = gasSum + measurement;
+        //}
+        //var gasAverage = gasSum / allMeasurements.length;
+        //console.log(luxAverage);
+
+        // Durchschnittlichen Lux-Wert in Prozent umrechnen und als Balken und Text anzeigen
+        //var levelAverage = gasAverage * (100 / maxLevel);
+        //var widthStyleAverage = "width: " + levelAverage + "%;"
+        //document.getElementById("gaslevel-average-bar").style = widthStyleAverage;
+        //document.getElementById("gaslevel-average-text").innerHTML = gasAverage.toFixed(2) + "Gasdurch"; // Auf 2 Nachkommastellen reduzieren
+
+        // Wert im Chart hinzufügen
+        //addData(gas);
     }
 }
 
-async function setCounter() {
-    // read the value from the input field
-    var counter = document.getElementById("counterinput").value;
+//////////////////////////////////
+/////   Code für das Chart   /////
+//////////////////////////////////
 
-    // call the function
-    var response = await axios.post(rootUrl + "/api/device/0/function/setCounter", { arg: counter });
+// Chart und Variablen 
+//var chartData, chartOptions, chart;
+//google.charts.load('current', { packages: ['corechart'] });
+//google.charts.setOnLoadCallback(drawChart);
 
-    // Handle the response from the server
-    alert("Response: " + response.data.result); // we could to something meaningful with the return value here ... 
-}
+// Chart initialisieren. Diese Funktion wird einmalig aufgerufen, wenn die Page geladen wurde.
+//function drawChart() {
+    // Daten mit dem Dummy-Wert ["", 0] initialisieren. 
+    // (Dieser Dummy-Wert ist nötig, damit wir das Chart schon anzeigen können, bevor 
+    // wir Daten erhalten. Es können keine Charts ohne Daten gezeichnet werden.)
+    //chartData = google.visualization.arrayToDataTable([['Time', 'Gasdurch'], ["", 0]]);
+    // Chart Options festlegen
+    //chartOptions = {
+        //title: 'Gas Level',
+        //hAxis: { title: 'Time' },
+        //vAxis: { title: 'Gas' },
+        //animation: {
+            //duration: 300, // Dauer der Animation in Millisekunden
+            //easing: 'out',
+        //},
+        //curveType: 'function', // Werte als Kurve darstellen (statt mit Strichen verbundene Punkte)
+        //legend: 'none',
+        //vAxis: {
+            // Range der vertikalen Achse
+            //viewWindow: {
+                //min: 0,
+                //max: maxLevel
+            //},
+        //}
+    //};
+    // LineChart initialisieren
+    //chart = new google.visualization.LineChart(document.getElementById('gaslevel-chart'));
+    //chartData.removeRow(0); // Workaround: ersten (Dummy-)Wert löschen, bevor das Chart zum ersten mal gezeichnet wird.
+    //chart.draw(chartData, chartOptions); // Chart zeichnen
+//}
 
-async function getCounter() {
-    // request the variable "counter"
-    var response = await axios.get(rootUrl + "/api/device/0/variable/counter");
-    var counter = response.data.result;
+// Eine neuen Wert ins Chart hinzufügen
+//function addData(gas) {
 
-    // update the html element
-    document.getElementById("counter").innerHTML = counter;
+    //if (allMeasurements.length > 10) {
+        // Älteste Messung in den Chartdaten entfernen 
+        //chartData.removeRow(0);
+    //}
+
+    // aktuelles Datum/Zeit
+    //var date = new Date();
+    // aktuelle Zeit in der Variable 'localTime' speichern
+    //var localTime = date.toLocaleTimeString();
+
+    // neuen Wert zu den Chartdaten hinzufügen
+    //chartData.addRow([localTime, gas]);
+
+    // Chart neu rendern
+    //chart.draw(chartData, chartOptions);
+//}
 }
