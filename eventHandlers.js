5d6cc3c7f4e3f03146610399f66537000049dd28
Duska12 <duska.dujakovic@gmx.ch>
2021-12-13 22:14:52 +0100

    changes

--- eventHandlers.js
+++ eventHandlers.js
@@ -8,27 +8,6 @@ exports.registerEventHandlers = function (source) {
     // Register more event handlers here
     }
     
-    function handleGas(event) {
-        // read variables from the event
-        var data = {
-            eventName: event.type,
-            eventData: JSON.parse(event.data).data, // the value of the event
-            deviceId: JSON.parse(event.data).coreid,
-            timestamp: JSON.parse(event.data).published_at
-        };
-    
-        try {        
-            // Log the event in the database
-            logger.logOne("MyDB", "Gasdurchschnitt", data);
-    
-            // send data to all connected clients
-            exports.sendEvent(data);
-        } catch (error) {
-            console.log("Could not handle event: " + JSON.stringify(event) + "\n");
-            console.log(error)
-        }
-    }
-
     function handleTemp(event) {
         // read variables from the event
         var data = {
@@ -41,6 +20,27 @@ exports.registerEventHandlers = function (source) {
         try {        
             // Log the event in the database
             logger.logOne("MyDB", "Temperaturdurchschnitt", data);
+    
+            // send data to all connected clients
+            exports.sendEvent(data);
+        } catch (error) {
+            console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+            console.log(error)
+        }
+    }
+
+    function handleGas(event) {
+        // read variables from the event
+        var data = {
+            eventName: event.type,
+            eventData: JSON.parse(event.data).data, // the value of the event
+            deviceId: JSON.parse(event.data).coreid,
+            timestamp: JSON.parse(event.data).published_at
+        };
+    
+        try {        
+            // Log the event in the database
+            logger.logOne("MyDB", "Gasdurchschnitt", data);
     
             // send data to all connected clients
             exports.sendEvent(data);
